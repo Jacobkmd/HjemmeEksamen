708b3796695a73b28d0dc3c2b9207021d8e0d628
Jacobkmd <56678539+Jacobkmd@users.noreply.github.com>
2020-10-28 21:36:54 +0100

    Skrevet metoden fjern(T verdi) ferdig.

--- src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
+++ src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
@@ -109,7 +109,61 @@ public class EksamenSBinTre<T> {
 
 //Oppgave 6
     public boolean fjern(T verdi) {
+        if (verdi == null) return false;  // treet har ingen nullverdier
 
+        Node<T> p = rot, q = null;   // q skal være forelder til p
+
+        while (p != null)            // leter etter verdi
+        {
+            int cmp = comp.compare(verdi,p.verdi);      // sammenligner
+            if (cmp < 0) { q = p; p = p.venstre; }      // går til venstre
+            else if (cmp > 0) { q = p; p = p.høyre; }   // går til høyre
+            else break;    // den søkte verdien ligger i p
+        }
+        if (p == null) return false;   // finner ikke verdi
+
+        if (p.venstre == null || p.høyre == null)  // Tilfelle 1) og 2)
+        {
+            Node<T> b = p.venstre != null ? p.venstre : p.høyre;  // b for barn
+            if (p == rot) rot = b;
+            else if (p == q.venstre){
+                q.venstre = b;
+                b.forelder = q;
+            }
+            else{
+                q.høyre = b;
+                b.forelder = q;
+            }
+        }
+        else  // Tilfelle 3)
+        {
+            Node<T> s = p, r = p.høyre;   // finner neste i inorden
+            while (r.venstre != null)
+            {
+                s = r;    // s er forelder til r
+                r = r.venstre;
+            }
+
+            p.verdi = r.verdi;   // kopierer verdien i r til p
+
+            if (s != p) {
+                if(r.høyre != null){
+                    r.høyre.forelder = s;
+                    s.venstre = r.høyre;
+                }
+                s.venstre = null;
+            }
+            else {
+                if(r.høyre != null){
+                    r.høyre.forelder = s;
+                    s.høyre = r.høyre;
+                }
+                p.høyre = null;
+            }
+        }
+
+        antall--;   // det er nå én node mindre i treet
+        return true;
     }
 
     public int fjernAlle(T verdi) {
